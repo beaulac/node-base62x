181b9f3f143ee08070d9344b010eb5577dba5caa
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2017-12-23 16:47:26 -0500

    Add test with corrected(?) examples.

---
+++ test/index.js
@@ -0,0 +1,51 @@
+'use strict';
+const assert = require('assert');
+
+
+// https://github.com/wadelau/Base62x/blob/master/base62x.presentation.201107.pdf
+const examples = [
+    // ['A', 'GG'], // Reference encodes as `G1`
+    // ['Ab', 'GM8'], // Reference encodes as `GM2`
+    ['aBC', 'OK93']
+    // ['A__B*', 'GLx1VGYe'] // Reference encodes as `GLx1VGYA`
+];
+
+const correctedExamples = [
+    ['A', 'G1'],
+    ['Ab', 'GM2'],
+    ['aBC', 'OK93'],
+    ['A__B*', 'GLx1VGYA']
+];
+
+describe('Reference Implementation', function () {
+    const RefB62x = require('../original');
+
+    xdescribe(' with original examples', function () {
+        it('Encodes examples correctly', function () {
+            examples.forEach(
+                ([input, expected]) => assert.strictEqual(RefB62x.encode(input), expected)
+            );
+        });
+
+        it('Decodes examples correctly', function () {
+            examples.forEach(
+                ([expected, encoded]) => assert.strictEqual(RefB62x.decode(encoded), expected)
+            );
+        });
+    });
+
+    describe(' with corrected examples', function () {
+        it('Encodes examples correctly', function () {
+            correctedExamples.forEach(
+                ([input, expected]) => assert.strictEqual(RefB62x.encode(input), expected)
+            );
+        });
+
+        it('Decodes examples correctly', function () {
+            correctedExamples.forEach(
+                ([expected, encoded]) => assert.strictEqual(RefB62x.decode(encoded), expected)
+            );
+        });
+    });
+
+});
