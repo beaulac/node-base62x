7e3815300a3d796d508f224f7db4fc2747130c68
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2017-12-28 13:20:55 -0500

    Make decode more consistent with encode.

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 'use strict';
-/*
- * -Base62x in -JavaScript
- * Refers to
- 	http://ieeexplore.ieee.org/xpl/freeabs_all.jsp?arnumber=6020065
- 	-GitHub-Wadelau , base62x.c
- 	https://github.com/wadelau/Base62x
- 	https://ufqi.com/dev/base62x/?_via=-naturedns
- */
+
 const config = require('./lib/config');
 const encode = require('./lib/encode');
 const decode = require('./lib/decode');
@@ -14,6 +7,6 @@ const decode = require('./lib/decode');
 module.exports = {
     config,
     encode,
-    decode: decode.decode,
-    decodeString: decode.decodeString
+    decode,
+    decodeString: b62xInput => decode(b62xInput).toString()
 };
